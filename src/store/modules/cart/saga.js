924eb9abaf886611a66cd927a4f0092c662ee67e
DaniloArruda <da.ni.lo_arruda@outlook.com>
2019-09-23 01:48:21 -0300

    Code improvements

--- src/store/modules/cart/saga.js
+++ src/store/modules/cart/saga.js
@@ -1,41 +1,11 @@
 import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
+
 import api from '../../../services/api';
+import history from '../../../services/history';
 import { addToCartSuccess, updateAmountSuccess } from './actions';
 import { CART_ADD_REQUEST, CART_UPDATE_AMOUNT_REQUEST } from './actionTypes';
 import { formatPrice } from '../../../util/format';
-
-function* addToCart({ id }) {
-  const productExists = yield select(state =>
-    state.cart.find(p => p.id === id)
-  );
-
-  const stock = yield call(api.get, `/stock/${id}`);
-
-  const stockAmount = stock.data.amount;
-  const currentAmount = productExists ? productExists.amount : 0;
-
-  const amount = currentAmount + 1;
-
-  if (amount > stockAmount) {
-    toast.warn('Quantidade solicitada fora de estoque');
-    return;
-  }
-
-  if (productExists) {
-    yield put(updateAmountSuccess(id, amount));
-  } else {
-    const response = yield call(api.get, `/products/${id}`);
-
-    const data = {
-      ...response.data,
-      amount: 1,
-      priceFormatted: formatPrice(response.data.price),
-    };
-
-    yield put(addToCartSuccess(data));
-  }
-}
 
 function* updateAmount({ id, amount }) {
   if (amount <= 0) return;
@@ -49,6 +19,29 @@ function* updateAmount({ id, amount }) {
   }
 
   yield put(updateAmountSuccess(id, amount));
+}
+
+function* addToCart({ id }) {
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
+
+  if (productExists) {
+    const amount = productExists.amount + 1;
+    yield updateAmount({ id, amount });
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+
+    yield put(addToCartSuccess(data));
+
+    history.push('/cart');
+  }
 }
 
 export default all([
